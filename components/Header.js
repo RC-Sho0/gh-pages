95e2ba1b180be163a1726507eb61dadcf100fdd3
Võ Thành Hoàng Sơn <hoangson.vothanh@gmail.com>
2023-06-06 09:49:11 +0000

    update resume

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ const NavBar = () => {
     { id: 0, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 1, name: locale.NAV.PUBLICATION, to: '/tag/Publication' , show: true},
     { id: 2, name: locale.NAV.SEARCH, to: '/search', show: true},
-    { id: 2, name: locale.NAV.RESUME, to: 'https://drive.google.com/file/d/1XeQAMiJFKwxCXbcIrPjjfZW_cp-XeSnp/view?usp=sharing', show: true},
+    { id: 2, name: locale.NAV.RESUME, to: '/resume', show: true},
 
 
   ]
