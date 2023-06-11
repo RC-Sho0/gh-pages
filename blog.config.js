002c8fe18df1113523787727528e40136cc00524
Võ Thành Hoàng Sơn <hoangson.vothanh@gmail.com>
2023-06-11 13:42:03 +0000

    update domain

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'sowwn',
   author: 'Sho',
   email: 'hoangson.vothanh@gmail.com',
-  link: 'sowwn.dev',
+  link: 'https://www.sowwn.dev',
   description: 'On the road become a data scienctist',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Bangkok', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
