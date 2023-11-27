0b955da6864de98300e51003be0835d74a2ee77e
Võ Thành Hoàng Sơn <hoangson.vothanh@gmail.com>
2023-11-27 15:25:55 +0000

    271123

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Hoang Son-Vo Thanh',
   email: 'hoangson.vothanh@gmail.com',
   link: 'https://www.sowwn.dev',
-  description: '|AI/ML/CV Blog - Blog for student - Code with me| \nA blog and news platform for those with a passion for Artificial Intelligence, machine learning, and computer vision.',
+  description: '|AI/ML/CV Blog - Blog for student - Code with me|',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Bangkok', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
